a6c05331f4c43460958430376dc4c8c62a8e2685
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2026-08-26 02:11:06 +1200

    chore(storybook): use react vite package instead of next vite

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,9 +1,9 @@
-import type { StorybookConfig } from "@storybook/nextjs-vite";
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: ["@storybook/addon-a11y", "@storybook/addon-docs"],
   core: { disableTelemetry: true },
-  framework: "@storybook/nextjs-vite",
+  framework: "@storybook/react-vite",
 };
 export default config;
